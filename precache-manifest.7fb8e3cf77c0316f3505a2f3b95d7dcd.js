d3625e16af297c4b5b05ab08bd9127aa5929ec5a
Kai Jun Zhuang <eddieezhuang@gmail.com>
2020-09-02 13:36:51 -0700

    Updates

---
+++ precache-manifest.7fb8e3cf77c0316f3505a2f3b95d7dcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4cc0929a7ee6cc6c5c4c0035fc36f7d",
+    "url": "/scavhunt/index.html"
+  },
+  {
+    "revision": "f54c45e96f4f2da5ce36",
+    "url": "/scavhunt/static/css/main.3c2f2892.chunk.css"
+  },
+  {
+    "revision": "2e53068c94d19e41fb0c",
+    "url": "/scavhunt/static/js/2.16969c2b.chunk.js"
+  },
+  {
+    "revision": "e34b7f33815bf9d795cb0468cd433745",
+    "url": "/scavhunt/static/js/2.16969c2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f54c45e96f4f2da5ce36",
+    "url": "/scavhunt/static/js/main.c30b4c9b.chunk.js"
+  },
+  {
+    "revision": "04428b4358bb874cb357",
+    "url": "/scavhunt/static/js/runtime-main.69556e6f.js"
+  },
+  {
+    "revision": "4fa3683005fa20dd058f47b9cfd411df",
+    "url": "/scavhunt/static/media/FjallaOne-Regular.4fa36830.ttf"
+  }
+]);
